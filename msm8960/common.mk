1d895f9faa736fb4f5f866240c6430fa8ab28790
Josue Rivera <prbassplayer@gmail.com>
2015-04-08 19:17:03 -0600

    Lets not assume kernel source is always present
      * Mako uses prebuilt because Google is stupid
    
    Change-Id: Ic35dbbfeb7b2694d5cae52f3d8b88fef55ef4891
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- msm8960/common.mk
+++ msm8960/common.mk
@@ -29,7 +29,9 @@ common_deps  :=
 kernel_includes :=
 
 # Executed only on QCOM BSPs
-#ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(TARGET_PREBUILT_KERNEL,KERNEL_BIN),false)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-#endif
+endif
+endif
