c5dea5b18a4691b3427a5ea41229a0eb29603e3e
Josue Rivera <prbassplayer@gmail.com>
2015-04-25 18:37:02 -0600

    Revert "Lets not assume kernel source is always present"
      * Not acting 100% as expected and no longer needed anyway
    
    This reverts commit 1d895f9faa736fb4f5f866240c6430fa8ab28790.
    
    Change-Id: I6d1d01b9a742812989e32b2b23464407a8e83f39
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- msm8960/common.mk
+++ msm8960/common.mk
@@ -29,9 +29,7 @@ common_deps  :=
 kernel_includes :=
 
 # Executed only on QCOM BSPs
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-ifeq ($(TARGET_PREBUILT_KERNEL,KERNEL_BIN),false)
+#ifeq ($(call is-vendor-board-platform,QCOM),true)
     common_deps += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
     kernel_includes += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-endif
-endif
+#endif
